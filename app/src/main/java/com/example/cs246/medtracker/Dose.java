724574e041c7917440fabdedb6f6d520cc2265f9
Dwood <dwood15@gmail.com>
2016-02-24 18:20:36 -0700

    Updated skeleton functions for the medication Class

--- app/src/main/java/com/example/cs246/medtracker/Dose.java
+++ app/src/main/java/com/example/cs246/medtracker/Dose.java
@@ -2,13 +2,17 @@ package com.example.cs246.medtracker;
 
 import java.util.Date;
 
-/**
+/*
  * Created by Tim on 2/23/2016.
  */
 public class Dose {
     private Medication medication;
     private float amountDose;
     private String directions;
-    public Date doseDate;
+    private Date doseDate;
 
+    public void scheduleDose(Date doseDateToadd)
+    {
+        this.doseDate = doseDateToadd;
+    }
 }
--- app/src/main/java/com/example/cs246/medtracker/Medication.java
+++ app/src/main/java/com/example/cs246/medtracker/Medication.java
@@ -1,9 +1,25 @@
 package com.example.cs246.medtracker;
 
 /**
- * Created by Tim on 2/23/2016.
+ * Medication Class - Used for tracking a specific
+ * medication a user will be taking
  */
 public class Medication {
     private String name;
     private String unitType;
+
+    public Medication(String name, String unitType)
+    {
+        this.name = name;
+        this.unitType = unitType;
+    }
+    public String getUnitType()
+    {
+        return this.unitType;
+    }
+
+    public String getMedicationName()
+    {
+        return this.name;
+    }
 }
